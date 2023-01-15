f884b87ac1c692fd3115eced91d39e5af9453b07
QubaPra <116064097+QubaPra@users.noreply.github.com>
2023-01-15 01:31:17 +0100

    simple tree added

---
+++ script.js
@@ -0,0 +1,36 @@
+var squares = document.getElementsByTagName("div");
+var money = document.getElementById("money-feniksy");
+var moneyValue;
+if(localStorage.getItem("money") === null){
+  moneyValue = 200;
+}else{
+  moneyValue = parseInt(localStorage.getItem("money"));
+}
+money.innerHTML = "Feniksy has: $" + moneyValue;
+
+for (var i = 0; i < squares.length; i++) {
+  if(squares[i].id.startsWith("square")){
+    if(localStorage.getItem(squares[i].id) === "blue"){
+      squares[i].style.backgroundColor = "blue";
+    }else{
+      squares[i].style.backgroundColor = "red";
+    }
+    squares[i].addEventListener("click", function() {
+      if (moneyValue >= 10) {
+        if (this.style.backgroundColor === "red") {
+          this.style.backgroundColor = "blue";
+          moneyValue -= 10;
+          money.innerHTML = "Feniksy has: $" + moneyValue;
+          localStorage.setItem("money", moneyValue);
+          localStorage.setItem(this.id, "blue");
+          this.removeEventListener("click", arguments.callee);
+        } else {
+          this.style.backgroundColor = "red";
+          localStorage.setItem(this.id, "red");
+        }
+      }else{
+        alert("You have not enough money to buy this skill");
+      }
+    });
+  }
+}
